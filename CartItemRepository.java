591a9abe61f3e10a9d3508409d71bcc29c35f3e3
tadepallipranavi101 <144901711+tadepallipranavi101@users.noreply.github.com>
2023-09-28 18:08:28 +0530

    Commit

---
+++ CartItemRepository.java
@@ -0,0 +1,9 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+   List<CartItem> findByUserId(Long userId);
+}
